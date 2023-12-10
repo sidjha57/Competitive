72040a04a7d4f0622a80a293fd77967db62c177f
Siddharth Jha <sidjha57@gmail.com>
2023-12-10 11:20:55 +0530

    chore: atcoder practice problems

--- C++/Practice/AdHoc/C_Minimize_Abs_2.cpp
+++ C++/Practice/AdHoc/C_Minimize_Abs_2.cpp
@@ -1,11 +1,10 @@
 //Siddharth Jha
 
+#include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
 //#include<ext/pb_ds/tree_policy.hpp>
 //#include <ext/pb_ds/trie_policy.hpp>
 //using namespace __gnu_pbds;
-#include <string>
-#include <iostream>
-#include <unordered_map>
 using namespace std;
 //typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 //typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
@@ -36,28 +35,22 @@ using namespace std;
 #define logarr(arr,a,b)          for(int z=(a);z<=(b);z++) cout<<(arr[z])<<" ";cout<<endl;
 template <typename T> T          gcd(T a, T b){if(a%b) return gcd(b,a%b);return b;}
 template <typename T> T          lcm(T a, T b){return (a*(b/gcd(a,b)));}
+vs tokenizer(string str,char ch) {std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;}
 
 void solve() {
-    int n=5; 
-    string s = "assss"; 
-    cin >> n >> s;
-    int ans = 0, cnt = 0;
-    char lastChar = s[0];
-    unordered_map<char,int> freq;
-    for (int i=0; i<=n; i++) {
-        if (i<n && s[i] == lastChar) {
-            cnt++;
-        } else {
-            freq[lastChar] = max(freq[lastChar], cnt);
-            cnt=1;    
-        }
-        lastChar = s[i];
+    int  D; cin >> D;
+    int  ans = INT_MAX;
+
+    for (int x=1; ; x++) {
+        if ((x * x) > D) break;
+        ans  = min(ans, abs(D - x*x));
+        int y = sqrt((D - x*x));
+        ans = min(ans, abs(x*x + y*y -D));
+        y++;
+        ans = min(ans, abs(x*x + y*y - D));
     }
 
-    for (auto it: freq) {
-        ans += it.second;
-    }
-    cout << ans << "\n";
+    cout << ans;
 }
 
 int main(int argc, char const *argv[]){
@@ -66,7 +59,7 @@ int main(int argc, char const *argv[]){
         //freopen("input.txt", "r", stdin);
         //freopen("output.txt", "w", stdout);
   //#endif
-    ll t=1;
+    int t=1;
     while(t--){
      solve();
     }
